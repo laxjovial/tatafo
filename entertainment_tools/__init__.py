95219e1b930c224e7e5878273c02368ba24b29a4
laxjovial <laxjovial@gmail.com>
2025-07-04 15:58:52 -0700

    Create __init__.py

---
+++ entertainment_tools/__init__.py
@@ -0,0 +1,4 @@
+# domain_tools/entertainment_tools/__init__.py
+
+# This file marks the 'entertainment_tools' directory as a Python package.
+# It will contain tools specific to entertainment information retrieval.
