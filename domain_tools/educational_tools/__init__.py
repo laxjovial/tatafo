322f53a96747d5fe224f186e168756941dac30f8
laxjovial <laxjovial@gmail.com>
2025-07-04 15:44:18 -0700

    Create __init__.py

---
+++ domain_tools/educational_tools/__init__.py
@@ -0,0 +1,4 @@
+# domain_tools/education_tools/__init__.py
+
+# This file marks the 'education_tools' directory as a Python package.
+# It will contain tools specific to educational information retrieval.
