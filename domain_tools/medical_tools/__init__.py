05d90e6d39ee4678d55c76759a3d78ed3f5281f8
laxjovial <laxjovial@gmail.com>
2025-07-04 14:48:55 -0700

    Create __init__.py

---
+++ domain_tools/medical_tools/__init__.py
@@ -0,0 +1,4 @@
+# domain_tools/medical_tools/__init__.py
+
+# This file marks the 'medical_tools' directory as a Python package.
+# It will contain tools specific to medical data and information retrieval.
